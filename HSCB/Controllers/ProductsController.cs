0c9c9e192754dbc064192af53bfe661a86250639
Đặng Nguyễn Thế Hưng <dangnguyenthehung@gmail.com>
2017-11-21 23:30:03 +0700

    Update code

---
+++ HSCB/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HSCB.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            if (id > 0)
+            {
+                ViewBag.id = id;
+            }
+
+            return View();
+        }
+    }
+}
