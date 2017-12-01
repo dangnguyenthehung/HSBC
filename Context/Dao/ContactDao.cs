ab08acc7fd2d7d6420c749e72990f76129e6d355
Bao <baophan0209@outlook.com>
2017-12-01 23:57:00 +0700

    Update contact admin

---
+++ Context/Dao/ContactDao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList;
+using Context.Database;
+
+namespace Context.Dao
+{
+    public class ContactDao
+    {
+        hscbEntities db = null;
+        public ContactDao()
+        {
+            db = new hscbEntities();
+        }
+        public IEnumerable<Contact> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<Contact> model = db.Contacts;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Name.Contains(searchString) || x.Name.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.ID).ToPagedList(page, pageSize);
+        }
+        public Contact ViewDetail(int id)
+        {
+            return db.Contacts.Find(id);
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                var contact = db.Contacts.Find(id);
+                db.Contacts.Remove(contact);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
