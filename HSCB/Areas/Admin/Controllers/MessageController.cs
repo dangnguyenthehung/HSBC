c7dab7cd0d0a3aae0dad34d6655912254be44ecb
Đặng Nguyễn Thế Hưng <dangnguyenthehung@gmail.com>
2018-01-11 16:02:49 +0700

    update

---
+++ HSCB/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HSCB.Areas.Admin.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Admin/Message
+        public ActionResult Index(string message)
+        {
+            ViewBag.message = message;
+            return View();
+        }
+    }
+}
